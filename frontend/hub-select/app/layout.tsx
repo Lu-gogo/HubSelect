a7008876cb6cd2b5e266cee03cb84538cc7e95e6
Lu-gogo <XI-lank@outlook.com>
2026-01-10 13:59:16 +0800

    shadcn global custom

--- frontend/hub-select/app/layout.tsx
+++ frontend/hub-select/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// 1. 导入 Script 组件
+import Script from "next/script";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,9 +28,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-sans`}
       >
         {children}
+
+      {/* 2. 在 body 结束前注入脚本 */}
+        <Script
+          id="live-preview"
+          src="https://tweakcn.com/live-preview.min.js"
+          strategy="afterInteractive" // 在页面交互后加载，不影响首屏速度
+          crossOrigin="anonymous"
+        />
       </body>
     </html>
   );
